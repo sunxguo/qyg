4a66030ab701280f31e70bf18ab721da962e5b6b
MaTengfei <1042930803@qq.com>
2016-03-12 11:27:36 +0800

    update

---
+++ admin/templates/default/aboutus.php
@@ -0,0 +1,18 @@
+<div class="page">
+  <table class="table tb-type2">
+    <thead>
+      <tr class="space">
+        <th colspan="10" class="nobg big">关于我们</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td class="w12"></td>
+        <td>欢迎使风度商城系统，祝君生意兴隆。<a href="http://fengdukeji.com" title="技术支持" target="_blank">技术支持</a></td>
+      </tr>
+    </tbody><tfoot>
+      <tr class="tfoot">
+        <td colspan="10"></td>
+      </tr>
+    </tfoot>
+  </table>
