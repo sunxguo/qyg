92f22bc2f691992ad7b716f6249b7d6a852ccdcb
MaTengfei <1042930803@qq.com>
2016-03-22 16:24:50 +0800

    update

--- wap/js/tmpl/common-top.js
+++ wap/js/tmpl/common-top.js
@@ -3,7 +3,7 @@ $(function (){
 	var tmpl = '<div class="header-wrap">'
 	        		+'<a href="javascript:history.back();" class="header-back" ><span>返回</span></a>'
 						+'<h2>'+headTitle+'</h2>'
-						+'<a href="javascript:void(0)" id="btn-opera" class="i-main-opera" >'
+						+'<a href="javascript:void(0)" id="btn-opera" class="i-main-opera"  style="margin-right:0px;">'
 					 	+'<span></span>'
 				 	+'</a>'
     			+'</div>'
