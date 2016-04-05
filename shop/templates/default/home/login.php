0124f960d9a9e52dbe33d219c899a800075eeafe
MaTengfei <1042930803@qq.com>
2016-04-05 19:45:20 +0800

    update

--- shop/templates/default/home/login.php
+++ shop/templates/default/home/login.php
@@ -44,7 +44,7 @@
           <dt><?php echo $lang['login_index_checkcode'];?></dt>
           <dd style="min-height:54px;">
               <input type="text" name="captcha" autocomplete="off" class="text w50 fl" id="captcha" maxlength="4" size="10" />
-            <img src="<?php echo SHOP_SITE_URL?>/control/seccodes.php"/ > <a href="javascript:void(0)" class="ml5" onclick="javascript:document.getElementById('codeimage').src='<?php echo SHOP_SITE_URL?>/control/seccodes.php?>&t=' + Math.random();"><?php echo $lang['login_index_change_checkcode'];?></a>
+            <img src="<?php echo SHOP_SITE_URL?>/control/seccodes.php" name="codeimage" border="0" id="codeimage" / > <a href="javascript:void(0)" class="ml5" onclick="javascript:document.getElementById('codeimage').src='<?php echo SHOP_SITE_URL?>/control/seccodes.php?>&t=' + Math.random();"><?php echo $lang['login_index_change_checkcode'];?></a>
             <label></label>
           </dd>
         </dl>
