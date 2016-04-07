ac5558d96b91b5f18e87000084b6194908f526b4
MaTengfei <1042930803@qq.com>
2016-04-07 10:20:25 +0800

    update

--- shop/control/login.php
+++ shop/control/login.php
@@ -447,50 +447,50 @@ class loginControl extends BaseHomeControl {
             );
             if($member_info) {
                 // 更新卖家登陆时间
-                $model_seller->editSeller(array('last_login_time' => TIMESTAMP), array('seller_id' => $seller_info['seller_id']));
+       //          $model_seller->editSeller(array('last_login_time' => TIMESTAMP), array('seller_id' => $seller_info['seller_id']));
 
-                $model_seller_group = Model('seller_group');
-                $seller_group_info = $model_seller_group->getSellerGroupInfo(array('group_id' => $seller_info['seller_group_id']));
+       //          $model_seller_group = Model('seller_group');
+       //          $seller_group_info = $model_seller_group->getSellerGroupInfo(array('group_id' => $seller_info['seller_group_id']));
 
-                $model_store = Model('store');
-                $store_info = $model_store->getStoreInfoByID($seller_info['store_id']);
+       //          $model_store = Model('store');
+       //          $store_info = $model_store->getStoreInfoByID($seller_info['store_id']);
 
-                $_SESSION['is_login'] = '1';
-                $_SESSION['member_id'] = $member_info['member_id'];
-                $_SESSION['member_name'] = $member_info['member_name'];
-    			$_SESSION['member_email'] = $member_info['member_email'];
-    			$_SESSION['is_buy']	= $member_info['is_buy'];
-    			$_SESSION['avatar']	= $member_info['member_avatar'];
+       //          $_SESSION['is_login'] = '1';
+       //          $_SESSION['member_id'] = $member_info['member_id'];
+       //          $_SESSION['member_name'] = $member_info['member_name'];
+    			// $_SESSION['member_email'] = $member_info['member_email'];
+    			// $_SESSION['is_buy']	= $member_info['is_buy'];
+    			// $_SESSION['avatar']	= $member_info['member_avatar'];
 
-                $_SESSION['grade_id'] = $store_info['grade_id'];
-                $_SESSION['seller_id'] = $seller_info['seller_id'];
-                $_SESSION['seller_name'] = $seller_info['seller_name'];
-                $_SESSION['seller_is_admin'] = intval($seller_info['is_admin']);
-                $_SESSION['store_id'] = intval($seller_info['store_id']);
-                $_SESSION['store_name']	= $store_info['store_name'];
-                $_SESSION['is_own_shop'] = (bool) $store_info['is_own_shop'];
-                $_SESSION['bind_all_gc'] = (bool) $store_info['bind_all_gc'];
-                $_SESSION['seller_limits'] = explode(',', $seller_group_info['limits']);
-                if($seller_info['is_admin']) {
-                    $_SESSION['seller_group_name'] = '管理员';
-                    $_SESSION['seller_smt_limits'] = false;
-                } else {
-                    $_SESSION['seller_group_name'] = $seller_group_info['group_name'];
-                    $_SESSION['seller_smt_limits'] = explode(',', $seller_group_info['smt_limits']);
-                }
-                if(!$seller_info['last_login_time']) {
-                    $seller_info['last_login_time'] = TIMESTAMP;
-                }
-                $_SESSION['seller_last_login_time'] = date('Y-m-d H:i', $seller_info['last_login_time']);
-                $seller_menu = $this->getSellerMenuList($seller_info['is_admin'], explode(',', $seller_group_info['limits']));
-                $_SESSION['seller_menu'] = $seller_menu['seller_menu'];
-                $_SESSION['seller_function_list'] = $seller_menu['seller_function_list'];
-                if(!empty($seller_info['seller_quicklink'])) {
-                    $quicklink_array = explode(',', $seller_info['seller_quicklink']);
-                    foreach ($quicklink_array as $value) {
-                        $_SESSION['seller_quicklink'][$value] = $value ;
-                    }
-                }
+       //          $_SESSION['grade_id'] = $store_info['grade_id'];
+       //          $_SESSION['seller_id'] = $seller_info['seller_id'];
+       //          $_SESSION['seller_name'] = $seller_info['seller_name'];
+       //          $_SESSION['seller_is_admin'] = intval($seller_info['is_admin']);
+       //          $_SESSION['store_id'] = intval($seller_info['store_id']);
+       //          $_SESSION['store_name']	= $store_info['store_name'];
+       //          $_SESSION['is_own_shop'] = (bool) $store_info['is_own_shop'];
+       //          $_SESSION['bind_all_gc'] = (bool) $store_info['bind_all_gc'];
+       //          $_SESSION['seller_limits'] = explode(',', $seller_group_info['limits']);
+       //          if($seller_info['is_admin']) {
+       //              $_SESSION['seller_group_name'] = '管理员';
+       //              $_SESSION['seller_smt_limits'] = false;
+       //          } else {
+       //              $_SESSION['seller_group_name'] = $seller_group_info['group_name'];
+       //              $_SESSION['seller_smt_limits'] = explode(',', $seller_group_info['smt_limits']);
+       //          }
+       //          if(!$seller_info['last_login_time']) {
+       //              $seller_info['last_login_time'] = TIMESTAMP;
+       //          }
+       //          $_SESSION['seller_last_login_time'] = date('Y-m-d H:i', $seller_info['last_login_time']);
+       //          $seller_menu = $this->getSellerMenuList($seller_info['is_admin'], explode(',', $seller_group_info['limits']));
+       //          $_SESSION['seller_menu'] = $seller_menu['seller_menu'];
+       //          $_SESSION['seller_function_list'] = $seller_menu['seller_function_list'];
+       //          if(!empty($seller_info['seller_quicklink'])) {
+       //              $quicklink_array = explode(',', $seller_info['seller_quicklink']);
+       //              foreach ($quicklink_array as $value) {
+       //                  $_SESSION['seller_quicklink'][$value] = $value ;
+       //              }
+       //          }
                 $this->recordSellerLog('登录成功');
                 redirect('index.php?act=seller_center');
             } else {
