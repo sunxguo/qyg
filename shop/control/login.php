f15022bf0ba164703e93274fb7440094f01ecd50
MaTengfei <1042930803@qq.com>
2016-04-07 10:13:35 +0800

    update

--- shop/control/login.php
+++ shop/control/login.php
@@ -419,24 +419,24 @@ class loginControl extends BaseHomeControl {
 	}
 
 	public function loginOp() {
-        //$result = chksubmit(true,true,'num');
+        // $result = chksubmit(true,true,'num');
         //var_dump($result);
         // if ($result){
-        //     // if ($result === -11){
-        //     //     showDialog('用户名或密码错误','','error');
-        //     // } 
+            // if ($result === -11){
+            //     showDialog('用户名或密码错误','','error');
+            // } 
 
-        //     // else
-        //     // if ($result === -12){
-        //     //     showDialog('验证码错误','','error');
-        //     // }
+            // else
+            // if ($result === -12){
+            //     showDialog('验证码错误','','error');
+            // }
         // } else {
         //     showDialog('非法提交','','error');
         // }
 
-        // $model_seller = Model('member');
-        // $seller_info = $model_seller->getSellerInfo(array('user_name' => $_POST['user_name']));
-        // if($seller_info) {
+        $model_seller = Model('member');
+        $seller_info = $model_seller->getMemberInfo(array('user_name' => $_POST['user_name']));
+        if($seller_info) {
 
             $model_member = Model('member');
             $member_info = $model_member->getMemberInfo(
@@ -496,9 +496,8 @@ class loginControl extends BaseHomeControl {
             } else {
                 showMessage('用户名密码错误', '', '', 'error');
             }
-        } 
-        // else {
-        //     showMessage('用户名密码错误', '', '', 'error');
-        // }
+        } else {
+            showMessage('用户名密码错误', '', '', 'error');
+        }
     }
 }
