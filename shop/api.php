5ee63b11393df562fcb2e353ac00f8122e8a2b00
MonkeyKing1024 <MonkeyKing1024@gmail.com>
2016-04-26 21:40:50 +0800

    update

--- shop/api.php
+++ shop/api.php
@@ -43,7 +43,7 @@ if($_GET['act'] == 'adv'){
 	define('SHOP_SITE_URL',$site_url);
 	if ($_GET['op'] == 'g'){
 		include 'api/qq/oauth/qq_callback.php';
-		$_SESSION["callback"]="http://quanyougo.com/wap";
+		$_SESSION["callback"]="http://quanyougo.com";
 	}else{
 		include 'api/qq/oauth/qq_login.php';
 	}
