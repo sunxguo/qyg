9d0c11636183e8e63772b36a8e252355dac6a360
MonkeyKing1024 <MonkeyKing1024@gmail.com>
2016-05-01 12:22:56 +0800

    update

--- chat/config/config.ini.php
+++ chat/config/config.ini.php
@@ -1,6 +1,7 @@
 <?php
 defined('InShopNC') or exit('Access Invalid!');
-
+define('NODE_SITE_URL','http://182.92.156.106:8090');
+define('CHAT_SITE_URL','http://quanyougo.com/chat');
 
 define('CHAT_TEMPLATES_URL',CHAT_SITE_URL.'/templates/default');
 define('CHAT_RESOURCE_URL',CHAT_SITE_URL.'/resource');
